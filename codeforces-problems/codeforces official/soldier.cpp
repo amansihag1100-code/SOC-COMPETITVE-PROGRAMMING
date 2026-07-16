466bccacd87c6e0b0989a770ba54b1cec193f473
amansihag1100-code <amansihag1100@gmail.com>
2026-07-16 07:00:43 +0530

    codeforces problems

---
+++ codeforces-problems/codeforces official/soldier.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long k, n, w;
+    cin >> k >> n >> w;
+
+    long long total = k * w * (w + 1) / 2;
+
+    if (total > n)
+        cout << total - n;
+    else
+        cout << 0;
+
+    return 0;
+}

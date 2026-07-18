72960ba8fa83702af988900a5343a81e6ce7514b
amansihag1100-code <amansihag1100@gmail.com>
2026-07-18 19:52:21 +0530

    codeforces few problems

---
+++ codeforces-problems/codeforces official/chat.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s, target = "hello";
+    cin >> s;
+
+    int j = 0;
+
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == target[j]) {
+            j++;
+        }
+        if (j == target.size()) {
+            break;
+        }
+    }
+
+    if (j == target.size())
+        cout << "YES";
+    else
+        cout << "NO";
+
+    return 0;
+}
---
+++ codeforces-problems/codeforces official/queue.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n, t;
+    cin >> n >> t;
+
+    string s;
+    cin >> s;
+
+    while (t--) {
+        int i = 0;
+        while (i < n - 1) {
+            if (s[i] == 'B' && s[i + 1] == 'G') {
+                swap(s[i], s[i + 1]);
+                i += 2;   // Skip the next position
+            } else {
+                i++;
+            }
+        }
+    }
+
+    cout << s;
+
+    return 0;
+}
